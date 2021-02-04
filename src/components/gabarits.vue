40e15c5573335d7e89053219f393865de4302333
PA <paleboucher@centrale-marseille.fr>
2021-02-04 15:39:10 +0100

    More models

--- src/components/gabarits.vue
+++ src/components/gabarits.vue
@@ -4,14 +4,26 @@
       <h2>Gabarits</h2>
     </div>
     <div>
+      <h3>Présentations</h3>
       <ul>
+        <li><a href="https://com.centrale-marseille.fr/charte_graphique/zip/ppt.zip">PowerPoint</a> <span class="text-smaller">(565,3 Ko)</span></li>
+
         <li><a href="" disabled>Word</a> <span class="text-smaller">(à venir)</span></li>
-        <li><a href="https://com.centrale-marseille.fr/charte_graphique/zip/ppt.zip">PowerPoint</a> <span class="text-smaller">(à venir)</span></li>
+
         <li><a href="" disabled>Keynote</a> <span class="text-smaller">(à venir)</span></li>
+
         <li><a href="" disabled>Libre Office</a> <span class="text-smaller">(à venir)</span></li>
+
         <li><a href="" disabled>Reveal.js</a> <span class="text-smaller">(à venir)</span></li>
+
         <li><a href="" disabled>LaTeX</a> <span class="text-smaller">(à venir)</span></li>
       </ul>
+    </div>
+    <div>
+      <h3>Fonds pour visioconférence</h3>
+      <p><span class="text-smaller">(à venir)</span></p>
+      <h3>Intro vidéo</h3>
+      <p><span class="text-smaller">(à venir)</span></p>
     </div>
   </div>
 </template>
