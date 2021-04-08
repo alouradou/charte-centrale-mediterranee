7603477ed5babd447d8dd4ab57d8a6b531369e55
PA <paleboucher@centrale-marseille.fr>
2021-04-08 16:19:32 +0200

    Fix route of LeDeck view

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Deck.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/LeDeck.vue')
   }
 ]
 
