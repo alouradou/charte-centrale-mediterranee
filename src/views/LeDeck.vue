64fbfb6c3ec1367604ff860c7ed39bb04b499333
PA <paleboucher@centrale-marseille.fr>
2021-04-09 17:27:05 +0200

    Le Deck

--- src/views/LeDeck.vue
+++ src/views/LeDeck.vue
@@ -1,21 +1,63 @@
 <template>
   <div :id="$options.name">
-    <div class="container">
-      <div class="content md:grid md:grid-cols-4 md:gap-6">
-        <div class="col-span-full"><h2>Couleurs</h2></div>
-        <Couleurs class="text col-span-3" :colors="colors"></Couleurs>
+    <div class="container light">
+      <div class="content">
+        <div class="chapter md:grid md:grid-cols-2 md:gap-6">
+          <div>
+            <h1>
+              <img src="../assets/img/le-deck/logo-Centrale-Marseille-Le-Deck.png" alt="logo Centrale Marseille Le Deck">
+            </h1>
+          </div>
+          <p class="pt-6">
+          </p>
+        </div>
       </div>
     </div>
     <div class="container">
       <div class="content md:grid md:grid-cols-4 md:gap-6">
-        <Signature 
-          class="text col-span-3"
-          logoUrl="img/logo-le-deck.png"
-          :logoWidth="101"
-          :logoHeight="51"
-          color1="#000f9f"
-          color2="#d4b718">
-        </Signature>
+        <div class="text col-span-3">
+          <div class="pb-6"><h2>Logo</h2></div>
+          <div class="chapter grid grid-cols-3 gap-6">
+            <div>
+              <img class="light" src="../assets/img/le-deck/logo-Centrale-Marseille-Le-Deck.png" alt="logo">
+            </div>
+            <div class="ledeck">
+              <img src="../assets/img/le-deck/logo-blanc-Centrale-Marseille-Le-Deck.png" alt="logo">
+            </div>
+            <div class="light">
+              <img src="../assets/img/le-deck/logo-jaune-Centrale-Marseille-Le-Deck.png" alt="logo">
+            </div>
+            <div class="dark">
+              <img src="../assets/img/le-deck/logo-blanc-Centrale-Marseille-Le-Deck.png" alt="logo">
+            </div>
+            <div class="light">
+              <img src="../assets/img/le-deck/logo-bleu-Centrale-Marseille-Le-Deck.png" alt="logo">
+            </div>
+            <div class="black">
+              <img src="../assets/img/le-deck/logo-blanc-Centrale-Marseille-Le-Deck.png" alt="logo">
+            </div>
+          </div>
+          <div class="col-span-full">
+            <h3>Pictogrammes</h3>
+            <div class="grid grid-cols-5">
+            <img class="light" src="../assets/img/le-deck/picto-Le-Deck.png" alt="picto">
+            <img class="ledeck" src="../assets/img/picto-blanc-zExcl.png" alt="picto">
+            <img class="light" src="../assets/img/le-deck/picto-jaune-Le-Deck.png" alt="">
+            <img class="dark" src="../assets/img/picto-blanc-zExcl.png" alt="picto">
+            <img class="light" src="../assets/img/picto-bleu-zExcl.png" alt="picto">
+            </div>
+          </div>
+          <div class="pt-6"><h2>Couleurs</h2></div>
+          <Couleurs :colors="colors"></Couleurs>
+          <Signature 
+            class="pt-6"
+            logoUrl="img/logo-le-deck.png"
+            :logoWidth="101"
+            :logoHeight="51"
+            color1="#000f9f"
+            color2="#d4b718">
+          </Signature>
+        </div>
       </div>
     </div>
   </div>
