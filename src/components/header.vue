071582f900704213de50db3bf6f2176547e720df
PA <paleboucher@centrale-marseille.fr>
2021-04-14 18:05:17 +0200

    Fix menu responsive

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,39 +1,37 @@
 <template>
   <header id="theHeader">
     <div class="container">
-      <div class="content grid sm:grid sm:grid-cols-2 sm:gap-2 md:grid-cols-4 md:gap-4 lg:grid-cols-6 lg:gap-6">
-        <div class="col-span-1">
+      <div class="content grid gap-2 sm:grid-cols-3 md:gap-4 lg:grid-cols-5 lg:gap-6">
+        <div class="col-span-1 logo-container">
           <a id="logo" :href="path">
             <img src="../assets/img/logo.png" alt="Logo">
           </a>
         </div>
-        
-        <nav id="nav" class="end nav-horizontal text-right col-span-5">
-          <button :class="{ active:isOpened }" class="burger-button" @click="toggleBurger" type="button" role="button" aria-label="open/close navigation"><i></i></button>
-          <ul :class="{ opened:isOpened }">
-            <li>
-              <router-link to="/">La marque</router-link>
-            </li>
-            <li>
-              <router-link to="/charte-graphique">La charte graphique</router-link>
-            </li>
-            <li>
-              <router-link to="/fondation">La fondation</router-link>
-            </li>
-            <li>
-              <router-link to="/alumni">Alumni</router-link>
-            </li>
-            <li>
-              <router-link to="/exectutive-education">Executive Education</router-link>
-            </li>
-            <li>
-              <router-link to="/le-deck">Le Deck</router-link>
-            </li>
-<!--             <li>
-              <router-link to="/deck">Le deck</router-link>
-            </li> -->
-          </ul>
-        </nav>
+        <div class="end text-left sm:col-span-3 lg:col-span-4">
+          <nav id="nav" class="nav-horizontal">
+            <button :class="{ active:isOpened }" class="burger-button" @click="toggleBurger" type="button" role="button" aria-label="open/close navigation"><i></i></button>
+            <ul :class="{ opened:isOpened }">
+              <li>
+                <router-link to="/">La marque</router-link>
+              </li>
+              <li>
+                <router-link to="/charte-graphique">La charte graphique</router-link>
+              </li>
+              <li>
+                <router-link to="/fondation">La fondation</router-link>
+              </li>
+              <li>
+                <router-link to="/alumni">Alumni</router-link>
+              </li>
+              <li>
+                <router-link to="/exectutive-education">Executive Education</router-link>
+              </li>
+              <li>
+                <router-link to="/le-deck">Le Deck</router-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   </header>
